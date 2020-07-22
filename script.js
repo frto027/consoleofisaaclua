17f013e2f3172e1c0997ebfb22fdda027253e586
frto027_win10 <602706150@qq.com>
2020-07-22 12:18:53 +0800

    update 1.9.0

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ let trans_json = {
         ['.s_request','申请'],
         ['.s_broken','失效'],
         ['.s_reset','重启'],
-        ['.copymark','已复制'],
-        ['.copy_card','复制'],
+        ['.copymark_console','已复制到控制台'],
+        ['.copymark_lua','已复制为lua'],
+        ['.copy_card','复制到控制台'],
+        ['.copy_card_lua','复制为lua'],
         ['#request_desc','这个脚本正在请求编写'],
         ['#broken_desc','这个脚本已经失效'],
         ['#reset_desc','在重新启动游戏（或重新开关任意mod）之前，脚本功能不会消失。这是因为部分lua指令会污染游戏的上下文，建议通过重新开关某个mod，或重启游戏程序的方式来进行重置。'],
@@ -58,12 +60,14 @@ function add_elem(data_item){
 
     //copy
     c_target = next_card.querySelector('.codes')
-    c_btn = next_card.querySelector('.copy_card')
-    c_target.id = 'copy_target_'+id
-    c_btn.setAttribute("data-clipboard-target", '#' + c_target.id)
 
-    c_mark = $(next_card.querySelector('.copymark'))
-    c_mark.id = 'copymark'+id
+    c_btn = next_card.querySelector('.copy_card')
+    c_btn.cptarget = c_target
+    
+    //copy lua
+    c_lua_btn = next_card.querySelector('.copy_card_lua')
+    c_lua_btn.cptarget = c_target
+
 
     set_data_item(data_item)
     nq = $(next_card)
@@ -115,23 +119,41 @@ function p(prop){
 }
 /* end === get data by language */
 
-
-
-let clipboard = new ClipboardJS('.copy_card')
+let clipboard = new ClipboardJS('.copy_card',{
+    text:(trigger)=>{
+        return trigger.cptarget.value
+    }
+})
 clipboard.on('success',(e)=>{
     $('.copymark').hide()
-    let c_mark = $(e.trigger.parentElement).children('.copymark')
+    let c_mark = $(e.trigger.parentElement).children('.copymark_console')
     c_mark.show('fast')
 })
 clipboard.on('error',()=>{
     $('.copymark').hide('fast')
     alert('copy failed')
 })
+
+
+let clipboard_lua = new ClipboardJS('.copy_card_lua',{
+    text:(trigger)=>{
+        return trigger.cptarget.value.substr(2)
+    }
+})
+clipboard_lua.on('success',(e)=>{
+    $('.copymark').hide()
+    let c_mark = $(e.trigger.parentElement).children('.copymark_lua')
+    c_mark.show('fast')
+})
+clipboard_lua.on('error',()=>{
+    $('.copymark').hide('fast')
+    alert('copy failed')
+})
+
 //show all element
 for(let i=0;i<data.length;i++){
     add_elem(data[i])
 }
-
 function keyword_is_match(keyword,keywords){
     keyword = keyword.toLowerCase()
     for(let i=0;i<keywords.length;i++){
@@ -141,7 +163,10 @@ function keyword_is_match(keyword,keywords){
     }
     return false
 }
-
+$('#search_input').on('keypress',(e)=>{
+    if(e.key=='Enter')
+        $('#search_btn').click()
+})
 $('#search_btn').on('click',()=>{
     clean_elems()
     search_text = $('#search_input').val().split(' ')
@@ -161,3 +186,6 @@ $('#search_btn').on('click',()=>{
 })
 
 $('#loading_div').remove()
+
+/* inform index.html */
+var script_execute_over
